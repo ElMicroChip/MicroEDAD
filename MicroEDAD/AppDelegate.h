62ff82956e070e8fcad423606e49a42882ffed04
Estudiante <mac_alfil_9@Mac-Alfil-9.local>
2014-08-14 17:54:31 -0500

    Initial Commit

---
+++ MicroEDAD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MicroEDAD
+//
+//  Created by Estudiante on 14/08/14.
+//  Copyright (c) 2014 Diego Zapata Murillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MicroEDAD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MicroEDAD
+//
+//  Created by Estudiante on 14/08/14.
+//  Copyright (c) 2014 Diego Zapata Murillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
